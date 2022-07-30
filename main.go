0ab41ff23d087b3a532b1492f11172b1f79a37f5
Le Viet Hoang <hashdotlee@gmail.com>
2022-07-30 17:05:04 +0700

    fix: cors positions

--- main.go
+++ main.go
@@ -16,40 +16,40 @@ func init(){
 }
 
 func main() {
-    r := gin.Default()
-	
+	r := gin.Default()
+
+	r.Use(cors.Default())
 	// Initialize the database.
 
-	  r.GET("/answers", controllers.GetAnswers)
-	  r.POST("/answers", middlewares.RequireAuth, controllers.CreateAnswer)
-	  r.GET("/answers/:id", controllers.GetAnswer)
-	  r.DELETE("/answers/:id", middlewares.RequireAuth, controllers.DeleteAnswer)
-	  r.POST("/answers/:id/vote", middlewares.RequireAuth, controllers.VoteAnswer)
-	  r.PATCH("/answers/:id/accept", middlewares.RequireAuth, controllers.AcceptAnswer)
-	  r.PUT("/answers/:id/update", middlewares.RequireAuth,controllers.UpdateAnswer)
+	r.GET("/answers", controllers.GetAnswers)
+	r.POST("/answers", middlewares.RequireAuth, controllers.CreateAnswer)
+	r.GET("/answers/:id", controllers.GetAnswer)
+	r.DELETE("/answers/:id", middlewares.RequireAuth, controllers.DeleteAnswer)
+	r.POST("/answers/:id/vote", middlewares.RequireAuth, controllers.VoteAnswer)
+	r.PATCH("/answers/:id/accept", middlewares.RequireAuth, controllers.AcceptAnswer)
+	r.PUT("/answers/:id/update", middlewares.RequireAuth,controllers.UpdateAnswer)
 
-	  r.GET("/questions", controllers.GetQuestions)
-	  r.POST("/questions", middlewares.RequireAuth, controllers.CreateQuestion)
-	  r.GET("/questions/:id", controllers.GetQuestion)
-	  r.DELETE("/questions/:id", middlewares.RequireAuth, controllers.DeleteQuestion)
-	  r.POST("/questions/:id/upvote", middlewares.RequireAuth, controllers.VoteQuestion)
-	  r.PUT("/questions/:id/update", middlewares.RequireAuth, controllers.UpdateQuestion)
+	r.GET("/questions", controllers.GetQuestions)
+	r.POST("/questions", middlewares.RequireAuth, controllers.CreateQuestion)
+	r.GET("/questions/:id", controllers.GetQuestion)
+	r.DELETE("/questions/:id", middlewares.RequireAuth, controllers.DeleteQuestion)
+	r.POST("/questions/:id/upvote", middlewares.RequireAuth, controllers.VoteQuestion)
+	r.PUT("/questions/:id/update", middlewares.RequireAuth, controllers.UpdateQuestion)
 
-	  r.POST("/signup", controllers.Signup)
-	  r.POST("/login", controllers.Login)
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
 
-	  r.PUT("/users/update", middlewares.RequireAuth, controllers.UpdateUser)
-	  r.GET("/users/info", middlewares.RequireAuth, controllers.GetUser)
-	  r.GET("/users", middlewares.RequireAdmin, controllers.GetUsers)
-	  r.PATCH("/user/verify", middlewares.RequireAdmin, controllers.VerifyUser)
+	r.PUT("/users/update", middlewares.RequireAuth, controllers.UpdateUser)
+	r.GET("/users/info", middlewares.RequireAuth, controllers.GetUser)
+	r.GET("/users", middlewares.RequireAdmin, controllers.GetUsers)
+	r.PATCH("/user/verify", middlewares.RequireAdmin, controllers.VerifyUser)
 
-	  r.GET("/topics", controllers.GetTopics)
-	  r.POST("/topics", middlewares.RequireAdmin, controllers.CreateTopic)
-	  r.GET("/topics/:id", controllers.GetTopic)
-	  r.DELETE("/topics/:id", middlewares.RequireAdmin, controllers.DeleteTopic)
-	  r.PUT("/topics/:id/update", middlewares.RequireAdmin, controllers.UpdateTopic)
+	r.GET("/topics", controllers.GetTopics)
+	r.POST("/topics", middlewares.RequireAdmin, controllers.CreateTopic)
+	r.GET("/topics/:id", controllers.GetTopic)
+	r.DELETE("/topics/:id", middlewares.RequireAdmin, controllers.DeleteTopic)
+	r.PUT("/topics/:id/update", middlewares.RequireAdmin, controllers.UpdateTopic)
 
-	  r.Use(cors.Default())
-	  r.Run()
-  }
+	r.Run()
+}
 
