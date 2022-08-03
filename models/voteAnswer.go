4024fb195bdbc38c6e019601bad4230f20ff0d81
Le Viet Hoang <hashdotlee@gmail.com>
2022-08-03 23:47:46 +0700

    add primary to vote

--- models/voteAnswer.go
+++ models/voteAnswer.go
@@ -15,8 +15,8 @@ type VoteAnswer struct {
 	ID uint `gorm:"autoIncrement:true" json:"id"`
 	CreatedAt time.Time `gorm:"autoCreateTime:true" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime:true" json:"updated_at"`
-	AnswerID uint `gorm:"primaryKey" json:"answer_id"`
-	UserID uint `json:"user_id" gorm:"primaryKey"`
+	AnswerID uint `gorm:"primaryKey;autoIncrement:false" json:"answer_id"`
+	UserID uint `json:"user_id" gorm:"primaryKey:autoIncrement:false"`
 	Type VoteAnswerType `json:"type"`
 }
 
--- models/voteQuestion.go
+++ models/voteQuestion.go
@@ -15,8 +15,8 @@ type VoteQuestion struct {
 	ID uint `gorm:"autoIncrement:true" json:"id"`
 	CreatedAt time.Time `gorm:"autoCreateTime:true" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime:true" json:"updated_at"`
-	QuestionID uint `gorm:"primaryKey" json:"answer_id"`
-	UserID uint `json:"user_id" gorm:"primaryKey"`
+	QuestionID uint `gorm:"primaryKey;autoIncrement:false" json:"answer_id"`
+	UserID uint `json:"user_id" gorm:"primaryKey:autoIncrement:false"`
 	Type VoteQuestionType `json:"type"`
 }
 
